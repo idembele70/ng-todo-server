4b020f7b52cc586500e30dbf336daaf07a4fdffb
idembele70 <idembele70@gmail>
2025-10-13 18:48:43 +0200

    chore(db): remove useless console log

--- database/index.db.js
+++ database/index.db.js
@@ -3,8 +3,6 @@ import mysql from 'mysql2/promise';
 const { DB_PROTOCOL, DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env
 const URL = `${DB_PROTOCOL}//${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
-console.log(URL);
-
 async function query(sql, params) {
   const connection = await mysql.createConnection(URL)
   const [results] = await connection.query(sql, params);
