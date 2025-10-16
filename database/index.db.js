e9866b0e65c020387511dae6e67640861223b986
idembele70 <idembele70@gmail>
2025-10-16 21:47:39 +0200

    feat(db): refactor database connection to use connection pool for improved performance

--- database/index.db.js
+++ database/index.db.js
@@ -1,11 +1,28 @@
 import mysql from 'mysql2/promise';
 
-const { DB_PROTOCOL, DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env
-const URL = `${DB_PROTOCOL}//${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_NAME,
+  POOL_CONNECTION_LIMIT,
+  POOL_QUEUE_LIMIT,
+ } = process.env
+
+ const pool = mysql.createPool({
+   host: DB_HOST,
+   user: DB_USER,
+   password: DB_PASSWORD,
+   database: DB_NAME,
+   port: Number(DB_PORT),
+   waitForConnections: true,
+   connectionLimit: Number(POOL_CONNECTION_LIMIT),
+   queueLimit: Number(POOL_QUEUE_LIMIT),
+ });
 
 async function query(sql, params) {
-  const connection = await mysql.createConnection(URL);
-  const [results] = await connection.query(sql, params);
+  const [results] = await pool.query(sql, params);
   return results;
 }
 
