ad7d25df7b5b91bf127e19254a37780507b9a444
idembele70 <idembele70@gmail>
2025-10-22 10:36:53 +0200

    fix(app): update environment variable for server port to use APP_DOCKER_PORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import ipFilterMiddleware from './middleware/ip-filter.middleware.js';
 
 const app = express();
 
-const { FRONT_END_URL, APP_LOCAL_PORT } = process.env;
+const { FRONT_END_URL, APP_DOCKER_PORT } = process.env;
 
 
 app.use(cors({
@@ -23,6 +23,6 @@ app.use((err, _req, res, _next) => {
   res.status(500).send('An error occurred!');
 })
 
-app.listen(APP_LOCAL_PORT, () => {
-  console.log(`Server running on port: ${APP_LOCAL_PORT}`);
+app.listen(APP_DOCKER_PORT, () => {
+  console.log(`Server running on port: ${APP_DOCKER_PORT}`);
 })
