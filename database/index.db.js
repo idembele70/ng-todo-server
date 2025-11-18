2b1a5406eb750d474194f5b030f53f9558043d40
idembele70 <idembele70@gmail>
2025-11-18 12:48:41 +0100

    fix: set local port for dev and docker port for prod

--- database/index.db.js
+++ database/index.db.js
@@ -5,22 +5,25 @@ const {
   DB_USER,
   DB_USER_PASSWORD,
   DB_DOCKER_PORT,
+  DB_LOCAL_PORT,
   DB_NAME,
   POOL_CONNECTION_LIMIT,
   POOL_QUEUE_LIMIT,
+  NODE_ENV
  } = process.env
+
+const DB_PORT = NODE_ENV === 'development' ? DB_LOCAL_PORT : DB_DOCKER_PORT;
 
  const pool = mysql.createPool({
    host: DB_HOST,
    user: DB_USER,
    password: DB_USER_PASSWORD,
    database: DB_NAME,
-   port: Number(DB_DOCKER_PORT),
+   port: Number(DB_PORT),
    waitForConnections: true,
    connectionLimit: Number(POOL_CONNECTION_LIMIT),
    queueLimit: Number(POOL_QUEUE_LIMIT),
  });
-
 async function query(sql, params) {
   const [results] = await pool.query(sql, params);
   return results;
